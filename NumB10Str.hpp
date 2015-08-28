3b6024b419f39dc6968ded6ab28460d872707760
Winsu <Winsu@users.noreply.github.com>
2015-08-28 18:26:09 +0200

    Create NumB10Str.hpp

---
+++ NumB10Str.hpp
@@ -0,0 +1,37 @@
+#include <istream>
+#include <ostream>
+#include <cstring>
+#include <string>
+#include <climits>
+#include <numeric>
+#include <limits>
+#include <cmath>
+
+using namespace std;
+
+typedef char           			chint;
+typedef chint*           		p_chint;
+typedef unsigned char           uchint;
+typedef uchint*					p_uchint;
+typedef signed char				schint;
+typedef schint*					p_schint;
+typedef int*                    p_int;
+typedef signed short int 		ssint;
+typedef ssint*					p_ssint;
+typedef signed int              sint;
+typedef sint*					p_sint;
+typedef signed long int         slint;
+typedef slint*					p_slint;
+typedef signed long long int 	sllint;
+typedef sllint*					p_sllint;
+typedef unsigned char			uchint;
+typedef unsigned char			uci; // por uso amplio   
+typedef uchint*					p_uchint;
+typedef unsigned short int 		usint;
+typedef usint*                  p_usint;
+typedef unsigned int            uint;
+typedef uint*                   p_uint;
+typedef unsigned long int       ulint;
+typedef ulint*					p_ulint;
+typedef unsigned long long int 	ullint;
+typedef ullint* 				p_ullint;

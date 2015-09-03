e2ba87bd8a0f4b8b58c8aa3dd2815686ea4fae2c
Winsu <Winsu@users.noreply.github.com>
2015-09-03 11:12:44 +0200

    Update DigBCD.hpp

--- DigBCD.hpp
+++ DigBCD.hpp
@@ -2,16 +2,16 @@
 
 enum DigBCD {c=uci(0),u=uci(1),d=uci(2),t=uci(3),q=uci(4),z=uci(5),x=uci(6),s=uci(7),o=uci(8),n=uci(9),E=uci(255)};
 
-typedef pair<DigBCD,DigBCD> N2DigsBCD;//The pair class couples together a pair of values, which may be of different types (T2 and T1)
-									//The individual values can be accessed through its public members first and second.
+typedef pair<DigBCD,DigBCD> N2DigsBCD;
+									
 const DigBCD & operator ++ (DigBCD &);
 DigBCD operator ++ (DigBCD &,int);
 const DigBCD & operator -- (DigBCD &);
 DigBCD operator -- (DigBCD &,int);
-DigBCD  operator ! (DigBCD a);// Complemento a nueve
-DigBCD operator - (DigBCD a);// Complemento a la Base 10
-const DigBCD & mC9 (DigBCD & a);// Complemento a nueve
-const DigBCD & mC10 (DigBCD & a);// Complemento a la Base 10
+DigBCD  operator ! (DigBCD a);
+DigBCD operator - (DigBCD a);
+const DigBCD & mC9 (DigBCD & a);
+const DigBCD & mC10 (DigBCD & a);
 N2DigsBCD operator + (DigBCD a,DigBCD b);
 N2DigsBCD operator - (DigBCD a,DigBCD b);
 N2DigsBCD operator * (DigBCD a,DigBCD b);
@@ -47,3 +47,4 @@ N2DigsBCD Dig2N2DigsBCD(DigBCD);
 
 istream & operator >> (istream &,DigBCD &);
 ostream & operator << (ostream &,DigBCD);
+
